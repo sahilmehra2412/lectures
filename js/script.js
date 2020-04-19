cc40a497b381b0f8c06ec1f3a006d49bf71cd9a2
Sahil Mehra <sahilmehra2412@gmail.com>
2020-04-19 20:38:52 +0530

    JS

---
+++ js/script.js
@@ -0,0 +1,43 @@
+// var str = "Hello World";
+
+// var num = '10';
+
+
+// let , const
+
+// for (var i = 0; i < 4; i++) {
+//     console.log(i);
+// }
+
+// console.log(i);
+
+// for (let j = 0; j < 4; j++) {
+//     console.log(j);
+// }
+
+// console.log(j);
+
+// const PI = 3.14;
+
+
+// console.log(PI);
+
+// function demo(msg) {
+//     console.log(`Hello ${msg}`);
+// }
+
+// demo("User");
+
+// let input = document.getElementById("input");
+
+// function showData() {
+//     console.log(input.classList);
+// }
+
+// let obj = {
+//     1: "Hello",
+//     2: "World",
+//     3: [1, 2, 3, 4, 5],
+// }
+
+// console.log(obj[3]);
